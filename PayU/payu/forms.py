c12a2032321d8f815d1977f4902ccff1fe54b9f0
Atul Bhouraskar <atul@yescourse.com>
2014-06-03 23:01:43 +1000

    Many changes to ensure it works with current PayUMoney site

--- PayU/payu/forms.py
+++ PayU/payu/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from payu.utils import generate_hash
+from payu.utils import generate_hash, verify_hash
 
 class PayUForm(forms.Form):
     # payu specific fields
@@ -38,8 +38,70 @@ class PayUForm(forms.Form):
     drop_category = forms.CharField(required=False)
     custom_note = forms.CharField(required=False)
     note_category = forms.CharField(required=False)
+    service_provider = forms.CharField(initial="payu_paisa")
     
-    def clean(self):
-        cleaned_data = super(PayUForm, self).clean()
-        cleaned_data['hash'] = generate_hash(cleaned_data)
-        return cleaned_data
+    def __init__(self, data, *args, **kwargs):        
+        self.merchant_salt = kwargs.pop('merchant_salt', None)
+        if 'hash' not in data:
+            data['hash'] = generate_hash(data, 
+                                         merchant_salt=self.merchant_salt)
+        hide_fields = kwargs.pop('hide_fields', False)
+        
+        data['service_provider'] = 'payu_paisa'
+        super(PayUForm, self).__init__(data, *args, **kwargs)
+        
+        if hide_fields:
+            for name, field in self.fields.items():
+                field.widget = forms.HiddenInput()
+
+    
+class PayUReturnForm(forms.Form):
+    mihpayid = forms.CharField()
+    mode = forms.CharField()
+    status = forms.CharField()
+    key = forms.CharField()
+    txnid = forms.CharField()
+    amount = forms.DecimalField(decimal_places=2)
+    discount = forms.DecimalField(decimal_places=2, required=False)
+    productinfo = forms.CharField()
+    firstname = forms.CharField()
+    lastname = forms.CharField(required=False)
+    address1 = forms.CharField(required=False)
+    address2 = forms.CharField(required=False)
+    city = forms.CharField(required=False)
+    state = forms.CharField(required=False)
+    country = forms.CharField(required=False)
+    zipcode = forms.CharField(required=False)
+    email = forms.EmailField()
+    phone = forms.CharField()
+    udf1 = forms.CharField(required=False)
+    udf2 = forms.CharField(required=False)
+    udf3 = forms.CharField(required=False)
+    udf4 = forms.CharField(required=False)
+    udf5 = forms.CharField(required=False)
+    hash =  forms.CharField()
+    error = forms.CharField(required=False)
+    PG_TYPE = forms.CharField()
+    bank_ref_num = forms.CharField()
+    shipping_firstname = forms.CharField(required=False)
+    shipping_lastname = forms.CharField(required=False)
+    shipping_address1 = forms.CharField(required=False)
+    shipping_address2 = forms.CharField(required=False)
+    shipping_city = forms.CharField(required=False)
+    shipping_state = forms.CharField(required=False)
+    shipping_country = forms.CharField(required=False)
+    shipping_zipcode = forms.CharField(required=False)
+    shipping_phone = forms.CharField(required=False)
+    shipping_phoneverified = forms.CharField(required=False)
+    unmappedstatus = forms.CharField()
+    payuMoneyId = forms.CharField()
+    
+    def __init__(self, data, *args, **kwargs):        
+        self.merchant_salt = kwargs.pop('merchant_salt', None)
+        super(PayUReturnForm, self).__init__(data, *args, **kwargs)
+    
+    def clean_hash(self):
+        hash = self.cleaned_data['hash']
+        if not verify_hash(self.data, merchant_salt=self.merchant_salt):
+            raise forms.ValidationError("Hash validation failed")
+        return hash
--- PayU/payu/utils.py
+++ PayU/payu/utils.py
@@ -4,20 +4,26 @@ KEYS = ('key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
         'udf1', 'udf2', 'udf3', 'udf4', 'udf5',  'udf6',  'udf7', 'udf8',
         'udf9',  'udf10')
 
-def generate_hash(data):
+def generate_hash(data, merchant_salt=None):
     keys = ('key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
             'udf1', 'udf2', 'udf3', 'udf4', 'udf5',  'udf6',  'udf7', 'udf8',
             'udf9',  'udf10')
+    if not merchant_salt:
+        merchant_salt = settings.PAYU_INFO.get('merchant_salt')
     hash = sha512('')
     for key in KEYS:
         hash.update("%s%s" % (str(data.get(key, '')), '|'))
-    hash.update(settings.PAYU_INFO.get('merchant_salt'))
+    hash.update(merchant_salt)
     return hash.hexdigest().lower()
 
-def verify_hash(data, SALT):    
+def verify_hash(data, merchant_salt=None):
+    if not merchant_salt:
+        merchant_salt = settings.PAYU_INFO.get('merchant_salt')
+    keys = list(KEYS)
     keys.reverse()
-    hash = sha512(settings.PAYU_INFO.get('merchant_salt'))
+    hash = sha512(merchant_salt)
     hash.update("%s%s" % ('|', str(data.get('status', ''))))
-    for key in KEYS:
+    for key in keys:
         hash.update("%s%s" % ('|', str(data.get(key, ''))))
     return (hash.hexdigest().lower() == data.get('hash'))
+    
